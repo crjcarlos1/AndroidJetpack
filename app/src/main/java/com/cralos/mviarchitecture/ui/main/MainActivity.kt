4833a9f23c28c420586db713f63904b64b001be3
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-14 18:57:35 -0500

    Added custom-bottom-nav-controller

--- app/src/main/java/com/cralos/mviarchitecture/ui/main/MainActivity.kt
+++ app/src/main/java/com/cralos/mviarchitecture/ui/main/MainActivity.kt
@@ -3,24 +3,64 @@ package com.cralos.mviarchitecture.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import com.cralos.mviarchitecture.R
 import com.cralos.mviarchitecture.ui.BaseActivity
 import com.cralos.mviarchitecture.ui.auth.AuthActivity
+import com.cralos.mviarchitecture.ui.main.account.ChangePasswordFragment
+import com.cralos.mviarchitecture.ui.main.account.UpdateAccountFragment
+import com.cralos.mviarchitecture.ui.main.blog.UpdateBlogFragment
+import com.cralos.mviarchitecture.ui.main.blog.ViewBlogFragment
+import com.cralos.mviarchitecture.util.BottomNavController
+import com.cralos.mviarchitecture.util.setUpNavigation
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @InternalCoroutinesApi
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), BottomNavController.NavGraphProvider,
+    BottomNavController.OnNavigationGraphChanged,
+    BottomNavController.OnNavigationReselectedListener {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private val bottomNavController by lazy(LazyThreadSafetyMode.NONE) {
+        BottomNavController(
+            this,
+            R.id.main_nav_host_fragment,
+            R.id.nav_blog, this, this
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        subscribeObservers()
-        tool_bar.setOnClickListener {
-            sessionManager.logout()
+        setupActionBar()
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.setUpNavigation(bottomNavController,this)
+
+        if (savedInstanceState == null){
+            bottomNavController.onNavigationItemSelected()
         }
+
+        subscribeObservers()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() = bottomNavController.onBackPressed()
+
+    private fun setupActionBar() {
+        setSupportActionBar(tool_bar)
     }
 
     fun subscribeObservers() {
@@ -45,5 +85,47 @@ class MainActivity : BaseActivity() {
             progress_bar.visibility = View.GONE
         }
     }
+
+    override fun getNavGraphId(itemId: Int) = when (itemId) {
+        R.id.nav_blog -> {
+            R.navigation.nav_blog
+        }
+        R.id.nav_account -> {
+            R.navigation.nav_account
+        }
+        R.id.nav_create_blog -> {
+            R.navigation.nav_create_blog
+        }
+        else -> {
+            R.navigation.nav_blog
+        }
+    }
+
+    override fun onGraphChange() {
+        // TODO("What needs to happen when the graph changes?")
+    }
+
+    override fun onReselectNavItem(navController: NavController, fragment: Fragment) =
+        when (fragment) {
+            is ViewBlogFragment -> {
+                navController.navigate(R.id.action_viewBlogFragment_to_blogFragment)
+            }
+            is UpdateBlogFragment -> {
+                navController.navigate(R.id.action_updateBlogFragment_to_blogFragment)
+            }
+
+            is UpdateAccountFragment -> {
+                navController.navigate(R.id.action_updateAccountFragment_to_accountFragment)
+            }
+
+            is ChangePasswordFragment -> {
+                navController.navigate(R.id.action_changePasswordFragment_to_accountFragment)
+            }
+
+            else -> {
+                //do nothing
+            }
+        }
+
 
 }
---
+++ app/src/main/java/com/cralos/mviarchitecture/util/BottomNavController.kt
@@ -0,0 +1,178 @@
+package com.cralos.mviarchitecture.util
+
+import android.app.Activity
+import android.content.Context
+import androidx.annotation.IdRes
+import androidx.annotation.NavigationRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.cralos.mviarchitecture.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+/**
+ * Class credit: Allan Veloso
+ * I took the concept from Allan Veloso and made alterations to fit our needs.
+ * https://stackoverflow.com/questions/50577356/android-jetpack-navigation-bottomnavigationview-with-youtube-or-instagram-like#_=_
+ * @property navigationBackStack: Backstack for the bottom navigation
+ */
+class BottomNavController(
+    val context: Context,
+    @IdRes val containerId: Int,
+    @IdRes val appStartDestinationId: Int,
+    val graphChangeListener: OnNavigationGraphChanged?,
+    val navGraphProvider: NavGraphProvider
+) {
+
+    private val TAG = "AppDebug"
+    lateinit var activity: Activity
+    lateinit var fragmentManager: FragmentManager
+    lateinit var navItemChangeListener: OnNavigationItemChenge
+    private val navigationBackStack = BackStack.of(appStartDestinationId)
+
+    init {
+        if (context is Activity) {
+            activity = context
+            fragmentManager = (activity as FragmentActivity).supportFragmentManager
+        }
+    }
+
+    fun onNavigationItemSelected(itemId: Int = navigationBackStack.last()): Boolean {
+        //replace fragment representing a navigation item
+        val fragment =
+            fragmentManager.findFragmentByTag(itemId.toString()) ?: NavHostFragment.create(
+                navGraphProvider.getNavGraphId(itemId)
+            )
+
+        fragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.fade_in,
+                R.anim.fade_out,
+                R.anim.fade_in,
+                R.anim.fade_out
+            )
+            .replace(containerId, fragment, itemId.toString())
+            .addToBackStack(null)
+            .commit()
+
+        //add to backstack
+        navigationBackStack.moveLast(itemId)
+
+        //update checked icon
+        navItemChangeListener.onItemChange(itemId)
+
+        //comunicate with activity
+        graphChangeListener?.onGraphChange()
+
+        return true
+    }
+
+    fun onBackPressed() {
+        val childFragmentManager =
+            fragmentManager.findFragmentById(containerId)!!.childFragmentManager
+        when {
+            childFragmentManager.popBackStackImmediate() -> {
+            }
+
+            //fragment backstack is empty so try to back on the navigation stack
+            navigationBackStack.size > 1 -> {
+                //remove last item for backstack
+                navigationBackStack.removeLast()
+
+                //update the container  with new fragment
+                onNavigationItemSelected()
+            }
+            // if the stack has only one  and it's not the navigation  home  we should ensure
+            //  that the aplication always leave from startDestination
+            navigationBackStack.last() != appStartDestinationId -> {
+                navigationBackStack.removeLast()
+                navigationBackStack.add(0, appStartDestinationId)
+                onNavigationItemSelected()
+            }
+            else -> activity.finish()
+        }
+    }
+
+    private class BackStack : ArrayList<Int>() {
+
+        companion object {
+            fun of(vararg elements: Int): BackStack {
+                val b = BackStack()
+                b.addAll(elements.toTypedArray())
+                return b
+            }
+        }
+
+        fun removeLast() = removeAt(size - 1)
+
+        fun moveLast(item: Int) {
+            remove(item)
+            add(item)
+        }
+
+    }
+
+    // for setting the checked icon in the bottom nav
+    interface OnNavigationItemChenge {
+        fun onItemChange(itemId: Int)
+    }
+
+    fun setOnItemNavigationChanged(listener: (itemId: Int) -> Unit) {
+        this.navItemChangeListener = object : OnNavigationItemChenge {
+            override fun onItemChange(itemId: Int) {
+                listener.invoke(itemId)
+            }
+        }
+    }
+
+    //get id for each graph
+    //ex: R.navigation.nav_blog
+    //ex: R.navigation.nav_create_blog
+    interface NavGraphProvider {
+        @NavigationRes
+        fun getNavGraphId(itemId: Int): Int
+    }
+
+    //execute when navigation graph changes
+    //ex: Select a new item on the bottom nav
+    //ex: Home -> Account
+    interface OnNavigationGraphChanged {
+        fun onGraphChange()
+    }
+
+    interface OnNavigationReselectedListener {
+        fun onReselectNavItem(navController: NavController, fragment: Fragment)
+    }
+
+}
+
+fun BottomNavigationView.setUpNavigation(
+    bottomNavController: BottomNavController,
+    onReselectListener: BottomNavController.OnNavigationReselectedListener
+) {
+
+    setOnNavigationItemSelectedListener {
+        bottomNavController.onNavigationItemSelected(it.itemId)
+    }
+
+    setOnNavigationItemReselectedListener {
+        bottomNavController
+            .fragmentManager
+            .findFragmentById(bottomNavController.containerId)!!
+            .childFragmentManager
+            .fragments[0].let { fragment ->
+            onReselectListener.onReselectNavItem(
+                bottomNavController.activity.findNavController(bottomNavController.containerId),
+                fragment
+            )
+        }
+    }
+
+    bottomNavController.setOnItemNavigationChanged { itemId ->
+        menu.findItem(itemId).isChecked = true
+    }
+
+}
