44e0faf82f167060fe15b76b9828eff7759753b5
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-07-17 17:43:39 -0500

    Added recyclerview-adapter-diffutil-and-list-update-callback

--- app/src/main/java/com/cralos/mviarchitecture/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/cralos/mviarchitecture/ui/main/blog/BlogFragment.kt
@@ -7,14 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 import com.cralos.mviarchitecture.R
+import com.cralos.mviarchitecture.models.BlogPost
 import com.cralos.mviarchitecture.ui.main.blog.state.BlogStateEvent
+import com.cralos.mviarchitecture.util.TopSpacingItemDecoration
 import kotlinx.android.synthetic.main.fragment_blog.*
 import kotlinx.coroutines.InternalCoroutinesApi
+import javax.inject.Inject
 
 @InternalCoroutinesApi
-class BlogFragment : BaseBlogFragment() {
+class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction {
 
+    @Inject
+    lateinit var requestManager: RequestManager
+
+    lateinit var recyclerAdapter: BlogListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,15 +37,18 @@ class BlogFragment : BaseBlogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        goViewBlogFragment.setOnClickListener {
-            findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
-        }
-
+        initRecyclerView()
         subscribeObservers()
         executeSearch()
     }
 
-    private fun executeSearch(){
+    override fun onDestroy() {
+        super.onDestroy()
+        //clear references (can leak memory)
+        blog_post_recyclerview.adapter = null
+    }
+
+    private fun executeSearch() {
         viewModel.setQuery("")
         viewModel.setStateEvent(BlogStateEvent.BlogSearchEvent())
     }
@@ -55,9 +68,48 @@ class BlogFragment : BaseBlogFragment() {
             }
         })
 
-        viewModel.viewState.observe(viewLifecycleOwner, Observer {viewState ->
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
             Log.d(TAG, "BlogFragment: viewState: $viewState")
+            if (viewState != null) {
+                recyclerAdapter.submitList(
+                    blogList = viewState.blogFields.blogList,
+                    isQueryExhausted = true
+                )
+            }
         })
+    }
+
+    private fun initRecyclerView() {
+        blog_post_recyclerview.apply {
+            layoutManager = LinearLayoutManager(this@BlogFragment.context)
+            val topSpacingItemDecoration = TopSpacingItemDecoration(30)
+            removeItemDecoration(topSpacingItemDecoration)
+            addItemDecoration(topSpacingItemDecoration)
+
+            recyclerAdapter = BlogListAdapter(
+                requestManager = requestManager,
+                interaction = this@BlogFragment
+            )
+
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val lastPosition = layoutManager.findLastVisibleItemPosition()
+                    if (lastPosition == recyclerAdapter.itemCount.minus(1)) {
+                        Log.d(TAG, "BlogFragment: attemting to load next page...")
+                        //TODO("Load next page using viewModel")
+                    }
+                }
+            })
+
+            adapter = recyclerAdapter
+
+        }
+    }
+
+    override fun onItemSelected(position: Int, item: BlogPost) {
+        Log.d(TAG, "onItemSelected: position,blogPost: $position, $item")
     }
 
 }
